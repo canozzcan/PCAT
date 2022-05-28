5f2220e4ff2dafb6bced3bed28c20b8bfced328d
canozzcan <canozcanstu@gmail.com>
2022-05-28 21:18:25 +0300

    Set MongoDb Connection

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
 
 const port = 3000;
 
 const app = express();
+
+// connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 //TEMPLATE ENGINE
 app.set("view engine", "ejs");
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 //ROUTES
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+    const photos = await Photo.find({});
+    res.render('index', {
+        photos: photos
+    });
 });
 
 app.get('/about', (req, res) => {
@@ -23,6 +36,11 @@ app.get('/about', (req, res) => {
 
 app.get('/add', (req, res) => {
     res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+    await Photo.create(req.body)
+    res.redirect('/');
 });
 
 
