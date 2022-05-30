499908493d3aba1097e4ba71f1fd93d4f095e28c
canozzcan <canozcanstu@gmail.com>
2022-05-30 19:46:25 +0300

    Upload - Update - Delete Photos & Design MVC Pattern

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
 const ejs = require('ejs');
-const path = require('path');
-const Photo = require('./models/Photo');
+const photoController = require('./controllers/photoControllers');
+const pageConroller = require('./controllers/pageController');
+
 
 const port = 3000;
 
@@ -19,29 +22,29 @@ app.set("view engine", "ejs");
 
 //MIDDLEWARES
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
+app.use(methodOverride('_method', {
+    methods: ['POST', 'GET']
+}));
 
 //ROUTES
-app.get('/', async (req, res) => {
-    const photos = await Photo.find({});
-    res.render('index', {
-        photos: photos
-    });
-});
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
 
-app.get('/about', (req, res) => {
-    res.render('about');
-});
+app.get('/about', pageConroller.getAboutPage);
+app.get('/add', pageConroller.getAddPage);
+app.get('/photos/edit/:id', pageConroller.getEditPage);
 
-app.get('/add', (req, res) => {
-    res.render('add');
-});
 
-app.post('/photos', async (req, res) => {
-    await Photo.create(req.body)
-    res.redirect('/');
-});
+
+
+
+
 
 
 app.listen(port, () => {
